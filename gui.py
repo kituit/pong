04877c6ae6129931ab8ff5012b2618634e0f3bae
Kit Glover <kglover@live.com.au>
2022-05-26 00:17:20 +1000

    Implemented movement of paddles

--- gui.py
+++ gui.py
@@ -11,9 +11,27 @@ START_COORDS_BALL = ((1.5 * SIZE) / 2  - BALL_SIZE, SIZE / 2 - BALL_SIZE)
 START_COORDS_LEFT = (0, SIZE / 2 - PADDLE_HEIGHT / 2)
 START_COORDS_RIGHT = (1.5 * SIZE - PADDLE_WIDTH, SIZE / 2 - PADDLE_HEIGHT / 2)
 
+GAME_COLOUR = (0, 0, 0)
+DISPLAY_COLOUR = (255, 255, 255)
+
+UP = -1
+DOWN = 1
+
+MOVEMENT_RATE = PADDLE_HEIGHT / 4
+
 class Paddle():
     def __init__(self, position):
         self.position = position # Co-ord of top left pixel
+
+    def move(self, move_type):
+        curr_height = self.position[1]
+        if move_type == UP:
+            new_height = max(0, curr_height - MOVEMENT_RATE)
+        else:
+            new_height = min(SIZE - PADDLE_HEIGHT, curr_height + MOVEMENT_RATE)
+        
+        self.position = (self.position[0], new_height)
+            
 
 class Ball():
     def __init__(self, position):
@@ -30,17 +48,31 @@ pygame.init()
 
 win = pygame.display.set_mode(DISPLAY_DIMENSIONS)
 pygame.display.set_caption("Pong")
-win.fill((255, 255, 255))
+win.fill(DISPLAY_COLOUR)
 pygame.display.update()
 
 cont = True
 while cont:
     ev = pygame.event.get()
+    keys = pygame.key.get_pressed()
     for event in ev:
         if event.type == pygame.QUIT:
             cont = False
-    pygame.draw.rect(win, (0, 0, 0), (*paddle_left.position, PADDLE_WIDTH, PADDLE_HEIGHT))
-    pygame.draw.rect(win, (0, 0, 0), (*paddle_right.position, PADDLE_WIDTH, PADDLE_HEIGHT))
-    pygame.draw.rect(win, (0, 0, 0), (*ball.position, BALL_SIZE, BALL_SIZE))
+
+    
+    if keys[pygame.K_UP]:
+        paddle_right.move(UP)
+    elif keys[pygame.K_DOWN]:
+        paddle_right.move(DOWN)
+    
+    if keys[pygame.K_w]:
+        paddle_left.move(UP)
+    elif keys[pygame.K_s]:
+        paddle_left.move(DOWN)
+
+    win.fill(DISPLAY_COLOUR)
+    pygame.draw.rect(win, GAME_COLOUR, (*paddle_left.position, PADDLE_WIDTH, PADDLE_HEIGHT))
+    pygame.draw.rect(win, GAME_COLOUR, (*paddle_right.position, PADDLE_WIDTH, PADDLE_HEIGHT))
+    pygame.draw.rect(win, GAME_COLOUR, (*ball.position, BALL_SIZE, BALL_SIZE))
     pygame.display.update()
-    sleep(0.5)
+    sleep(0.1)

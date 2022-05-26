21f9f84e350d5671a6b17a299a62f01a9716bcc3
Kit Glover <kglover@live.com.au>
2022-05-26 10:55:20 +1000

    Implemented basic ball physics

--- gui.py
+++ gui.py
@@ -2,14 +2,16 @@ from time import sleep
 import pygame
 
 SIZE = 1000
-DISPLAY_DIMENSIONS = (1.5 * SIZE, SIZE)
+DISPLAY_WIDTH = 1.5 * SIZE
+DISPLAY_HEIGHT = SIZE
+DISPLAY_DIMENSIONS = (DISPLAY_WIDTH, DISPLAY_HEIGHT)
 PADDLE_HEIGHT = SIZE / 5
 PADDLE_WIDTH = PADDLE_HEIGHT / 8
 BALL_SIZE = PADDLE_WIDTH
 
-START_COORDS_BALL = ((1.5 * SIZE) / 2  - BALL_SIZE, SIZE / 2 - BALL_SIZE)
+START_COORDS_BALL = (DISPLAY_WIDTH / 2  - BALL_SIZE, SIZE / 2 - BALL_SIZE)
 START_COORDS_LEFT = (0, SIZE / 2 - PADDLE_HEIGHT / 2)
-START_COORDS_RIGHT = (1.5 * SIZE - PADDLE_WIDTH, SIZE / 2 - PADDLE_HEIGHT / 2)
+START_COORDS_RIGHT = (DISPLAY_WIDTH - PADDLE_WIDTH, SIZE / 2 - PADDLE_HEIGHT / 2)
 
 GAME_COLOUR = (0, 0, 0)
 DISPLAY_COLOUR = (255, 255, 255)
@@ -17,11 +19,15 @@ DISPLAY_COLOUR = (255, 255, 255)
 UP = -1
 DOWN = 1
 
-MOVEMENT_RATE = PADDLE_HEIGHT / 4
+MOVEMENT_RATE = PADDLE_HEIGHT / 12
+BALL_SPEED_LIMIT = MOVEMENT_RATE
+SPEED_INCREASE_RATE = 1.5 # Rate at which ball increases speed when rebounds of paddle
+FPS = 60
 
-class Paddle():
+class Player():
     def __init__(self, position):
         self.position = position # Co-ord of top left pixel
+        self.score = 0
 
     def move(self, move_type):
         curr_height = self.position[1]
@@ -36,12 +42,55 @@ class Paddle():
 class Ball():
     def __init__(self, position):
         self.position = position
-
+        self.direction = (5, 5)
     
+    def move(self, player_left, player_right):
+        curr_x, curr_y = self.position
+        move_x, move_y = self.direction
+        new_x, new_y = curr_x, curr_y
 
-paddle_left = Paddle(START_COORDS_LEFT)
-paddle_right = Paddle(START_COORDS_RIGHT)
+        if curr_y + move_y < 0:
+            new_y = 0
+            move_y = -1 * move_y
+        elif curr_y + move_y > DISPLAY_HEIGHT - BALL_SIZE:
+            new_y = DISPLAY_HEIGHT - BALL_SIZE
+            move_y = -1 * move_y
+        else:
+            new_y = curr_y + move_y
+        
+        if curr_x + BALL_SIZE >= DISPLAY_WIDTH - PADDLE_WIDTH:
+            paddle_right_y = player_right.position[1]
+            if paddle_right_y <= curr_y <= paddle_right_y + PADDLE_HEIGHT or paddle_right_y <= curr_y + BALL_SIZE <= paddle_right_y + PADDLE_HEIGHT:
+                move_x = -1 * move_x
+                move_x, move_y = speed_up(move_x, move_y)
+            # Deal with losing point here
+        elif curr_x <= PADDLE_WIDTH:
+            paddle_left_y = player_left.position[1]
+            if paddle_left_y <= curr_y <= paddle_left_y + PADDLE_HEIGHT or paddle_left_y <= curr_y + BALL_SIZE <= paddle_left_y + PADDLE_HEIGHT:
+                move_x = -1 * move_x
+                move_x, move_y = speed_up(move_x, move_y)
+            # Deal with losing point here
+        else:
+            new_x = curr_x + move_x
+
+
+        self.direction = (move_x, move_y)
+        self.position = (curr_x + move_x, new_y)
+
+
+def speed_up(move_x, move_y):
+    if move_x ** 2 + move_y ** 2 >= BALL_SPEED_LIMIT ** 2:
+        return (move_x, move_y)
+    else:
+        max_speed_increase_rate = BALL_SPEED_LIMIT / ((move_x ** 2 + move_y ** 2) ** 0.5)
+        speed_increase = min(max_speed_increase_rate, SPEED_INCREASE_RATE)
+        return (move_x * speed_increase, move_y * speed_increase)
+
+player_left = Player(START_COORDS_LEFT)
+player_right = Player(START_COORDS_RIGHT)
 ball = Ball(START_COORDS_BALL)
+
+clock = pygame.time.Clock()
 
 # initialize all imported pygame modules
 pygame.init()
@@ -53,6 +102,9 @@ pygame.display.update()
 
 cont = True
 while cont:
+    clock.tick(FPS)
+    ball.move(player_left, player_right)
+
     ev = pygame.event.get()
     keys = pygame.key.get_pressed()
     for event in ev:
@@ -61,18 +113,17 @@ while cont:
 
     
     if keys[pygame.K_UP]:
-        paddle_right.move(UP)
+        player_right.move(UP)
     elif keys[pygame.K_DOWN]:
-        paddle_right.move(DOWN)
+        player_right.move(DOWN)
     
     if keys[pygame.K_w]:
-        paddle_left.move(UP)
+        player_left.move(UP)
     elif keys[pygame.K_s]:
-        paddle_left.move(DOWN)
+        player_left.move(DOWN)
 
     win.fill(DISPLAY_COLOUR)
-    pygame.draw.rect(win, GAME_COLOUR, (*paddle_left.position, PADDLE_WIDTH, PADDLE_HEIGHT))
-    pygame.draw.rect(win, GAME_COLOUR, (*paddle_right.position, PADDLE_WIDTH, PADDLE_HEIGHT))
+    pygame.draw.rect(win, GAME_COLOUR, (*player_left.position, PADDLE_WIDTH, PADDLE_HEIGHT))
+    pygame.draw.rect(win, GAME_COLOUR, (*player_right.position, PADDLE_WIDTH, PADDLE_HEIGHT))
     pygame.draw.rect(win, GAME_COLOUR, (*ball.position, BALL_SIZE, BALL_SIZE))
     pygame.display.update()
-    sleep(0.1)
